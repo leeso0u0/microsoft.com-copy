c993c5ed59751c434fd981ab4af788007464e64c
soyoung lee <decfugu@gmail.com>
2016-06-26 00:32:06 +0900

    favicon 추가/ stylus 맞춰서 styl 파일수정

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Microsoft 비공식 홈페이지' });
 });
+
+router.get('/test', function(req, res, next) {
+	res.render('test');
+})
 
 module.exports = router;
